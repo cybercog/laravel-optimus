df0088e77818c72957945143ee22a3fa7f93f580
Ivan Vermeyen <ivan@vermeyen.org>
2020-03-03 20:19:44 +0100

    Add support for Laravel 7

--- src/Traits/OptimusEncodedRouteKey.php
+++ src/Traits/OptimusEncodedRouteKey.php
@@ -20,10 +20,11 @@ trait OptimusEncodedRouteKey
      * Retrieve the model for a bound value.
      *
      * @param mixed $value
+     * @param string|null $field
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function resolveRouteBinding($value)
+    public function resolveRouteBinding($value, $field = null)
     {
         if (is_string($value) && ctype_digit($value)) {
             $value = (int) $value;
